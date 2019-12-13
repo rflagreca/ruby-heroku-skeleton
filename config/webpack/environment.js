ad29d96a190336b512c0c08d844a9d18638ea9ca
Roberto La Greca <roberto@rflagreca.me>
2019-12-13 15:11:58 +0000

    Add clean webpack plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const elm =  require('./loaders/elm')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-environment.loaders.prepend('elm', elm)
+environment.loaders.prepend('elm', elm);
+environment.plugins.prepend('CleanWebpackPlugin',
+                            new CleanWebpackPlugin());
 module.exports = environment
